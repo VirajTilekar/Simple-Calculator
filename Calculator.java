470314a4371297045943d1da987e0017d6ee04a7
Viraj Tilekar <67374433+VirajTilekar@users.noreply.github.com>
2021-09-21 15:22:58 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Calculator {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int button = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        switch (button) {
+            case 1 : System.out.println(a + b);
+                break;
+            case 2 : System.out.println(a - b);
+                break;
+            case 3 : System.out.println(a * b);
+                break;
+            case 4 : System.out.println(a / b);
+                break;
+            case 5 : System.out.println(a % b);
+                break;
+            default: System.out.println("Inavlid Button");
+                break;
+        }
+
+
+    }
+}
